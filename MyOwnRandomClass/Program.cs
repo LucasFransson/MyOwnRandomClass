d4d4a64542bbe6f43c08babcb128024a4f4e16a7
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-01-31 22:49:59 +0100

    Reimplemented Old Method

--- MyOwnRandomClass/Program.cs
+++ MyOwnRandomClass/Program.cs
@@ -1,125 +1,42 @@
-﻿
-using System.Diagnostics;
+﻿using MyOwnRandomClass;
 
 
 
-MyRandom random = new();
-List<int> intList = new List<int>();
 
+
+
+
+List<int> intList = new List<int>();     // Create an empty list to store the result from the RandomGenerator
 
 for (int i = 0; i < 100; i++)
 {
-    int test = random.Next(1, 100);
+    int test = RandomGenerator.GetRandom(1, 100);
     intList.Add(test);
     Console.WriteLine(test);
 }
-random.PrintNumberOccurrenceData(intList, 1, 100);
 
+RandomGenerator.PrintNumberOccurrenceData(intList, 1, 100);
+Console.ReadKey();
 
+intList.Clear();
 
-
-class MyRandom
+for (int i = 0; i < 100; i++)
 {
-    private int GetSeed()
-    {
-        return Environment.TickCount;
-    }
-    private int GetLastDigitOfInteger(int seedInt)
-    {
-        return (seedInt % 10);
-        //string tempSeedString = seedInt.ToString();    // Parse the int to a string
-        //char lastChar = tempSeedString.LastOrDefault();    //Set char lastChar to the last char of the string .Iow Set "lastNumber" to the last "number" of the "Integer"
-        //return int.Parse(tempSeedString);    // Parse the string back to an int and return it's value
-
-    }
-
-    public int Next(int low, int high)
-    {
-
-
-        int seedInt = GetSeed();
-        for (int i = 0; i < 3; i++)
-        {
-            seedInt = seedInt * seedInt.ToString().Last() / NumberOfDigits(seedInt, 0);
-        }
-        String randomSeedStringFull = seedInt.ToString();
-        int randomSeedInt = int.Parse(randomSeedStringFull.Substring(randomSeedStringFull.Length - NumberOfDigits(high, 0)));
-
-        int randomInt = low;
-        for (int i = 0; i <= randomSeedInt; i++)
-        {
-            randomInt++;
-            if (randomInt > high)
-            {
-                randomInt = low;
-            }
-        }
-        Thread.Sleep(1); // Change to StopWatch or use Async to free up thread 
-        return randomInt;
-
-
-    }
-
-    public void PrintNumberOccurrenceData(List<int> intList, int low, int high)
-    {
-        int nrCounter;
-        List<int> noOccurredInts = new();
-
-        Console.WriteLine($"Total amount of Numbers in list : {intList.Count()}");
-        for (int i = low; i <=high; i++)
-        {
-            if (intList.Contains(i))
-            {
-                
-                nrCounter = intList.FindAll(x => x == i).Count();
-                Console.WriteLine($"Number {i} Occurred : {nrCounter} Times");
-            }
-            else
-            {
-                noOccurredInts.Add(i);
-            }
-        }
-        Console.WriteLine("----------------------------");
-        Console.WriteLine($"The following numbers did not occur in the list: ");
-        if (noOccurredInts.Count < 1)
-        {
-            Console.Write("No numbers found. All numbers between input parameters occurred.");
-        }
-        else
-        {
-            foreach (int i in noOccurredInts)
-            {
-                    Console.Write($"[{i}], "); 
-            }
-        }
-        Console.WriteLine("\r\n----------------------------");
-        PrintExtremesData(intList, low, high);
-    }
-
-    private void PrintExtremesData(List<int> intList, int low, int high)
-    {
-        int nrCounter=0; 
-        int currentHighestCounter=0;
-        int currentHighestNr = 0;
-        for (int i = low;i<=high;i++)
-        {
-            nrCounter = intList.FindAll(x => x == i).Count();
-            if (currentHighestCounter < nrCounter)
-            {
-                currentHighestCounter = nrCounter;
-                currentHighestNr = i;
-            }
-        }
-        Console.WriteLine($"The most commonly occured number was {currentHighestNr}, which occured {intList.FindAll(x=>x==currentHighestNr).Count} Times.");
-
-    }
-
-    private int NumberOfDigits(int number, int noDigits)
-    {
-        if (number == 0)
-        {
-            return noDigits;
-        }
-        return NumberOfDigits(number / 10, ++noDigits);
-    }
+    int test = RandomGenerator.GetRandom(1, 1000);
+    intList.Add(test);
+    Console.WriteLine(test);
 }
+
+RandomGenerator.PrintNumberOccurrenceData(intList, 1, 1000);
+Console.ReadKey();
+
+intList.Clear();  
+
+for (int i = 0; i <1000; i++)
+{
+    int test = RandomGenerator.GetRandom(1, 100000);
+    intList.Add(test);
+    Console.WriteLine(test);
+}
+
+RandomGenerator.PrintNumberOccurrenceData(intList, 1, 100000);
---
+++ MyOwnRandomClass/RandomGenerator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnRandomClass
+{
+    public static class RandomGenerator
+    {
+        private static int GetSeed()
+        {
+            return Environment.TickCount;       // MilliSeconds since system started
+        }
+
+        // Returns the last digit of an Integer.    This Method is used in an attempt to randomize the seed Quickly without the need/risk for extremely big iterations.
+        private static int GetLastDigitOfInteger(int seedInt)
+        {
+            return (seedInt % 10);
+
+        }
+        // Returns the amount of digits in 'number'
+        private static int GetDigitsAmountOfInteger(int number, int noDigits)
+        {
+            if (number == 0)
+            {
+                return noDigits;
+            }
+            return GetDigitsAmountOfInteger(number / 10, ++noDigits);
+        }
+
+        public static int GetRandom(int low, int high)
+        {
+
+
+            int seedInt = GetSeed();
+            for (int i = 0; i < 3; i++)
+            {
+                seedInt = seedInt * seedInt.ToString().Last() / GetDigitsAmountOfInteger(seedInt, 0);
+            }
+            String randomSeedStringFull = seedInt.ToString();
+            int randomSeedInt = int.Parse(randomSeedStringFull.Substring(randomSeedStringFull.Length - GetDigitsAmountOfInteger(high, 0)));
+
+            int randomInt = low;
+            for (int i = 0; i <= randomSeedInt; i++)
+            {
+                randomInt++;
+                if (randomInt > high)
+                {
+                    randomInt = low;
+                }
+            }
+            Thread.Sleep(1); // Sleep for 1 millisecond to prevent the same Value returned if called quickly
+            return randomInt;
+
+        }
+
+
+        // Prints Number Occurence Data for Analyzing purposes to check large amounts of outputs
+        public static void PrintNumberOccurrenceData(List<int> intList, int low, int high)
+        {
+            int nrCounter;
+            List<int> noOccurredInts = new();
+
+            Console.WriteLine($"Total amount of Numbers in list : {intList.Count()}");
+            for (int i = low; i <= high; i++)
+            {
+                if (intList.Contains(i))
+                {
+
+                    nrCounter = intList.FindAll(x => x == i).Count();
+                    Console.WriteLine($"Number {i} Occurred : {nrCounter} Times");
+                }
+                else
+                {
+                    noOccurredInts.Add(i);
+                }
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine($"The following numbers did not occur in the list: ");
+            if (noOccurredInts.Count < 1)
+            {
+                Console.Write("No numbers found. All numbers between input parameters occurred.");
+            }
+            else
+            {
+                foreach (int i in noOccurredInts)
+                {
+                    Console.Write($"[{i}], ");
+                }
+            }
+            Console.WriteLine("\r\n----------------------------");
+            PrintExtremesData(intList, low, high);
+        }
+
+        // Prints The Extremes of the output Data for Analyzing purposes to check for repeating patterns or obvious errors.
+        private static void PrintExtremesData(List<int> intList, int low, int high)
+        {
+            int nrCounter = 0;
+            int currentHighestCounter = 0;
+            int currentHighestNr = 0;
+            for (int i = low; i <= high; i++)
+            {
+                nrCounter = intList.FindAll(x => x == i).Count();
+                if (currentHighestCounter < nrCounter)
+                {
+                    currentHighestCounter = nrCounter;
+                    currentHighestNr = i;
+                }
+            }
+            Console.WriteLine($"The most commonly occured number was {currentHighestNr}, which occured {intList.FindAll(x => x == currentHighestNr).Count} Times.");
+
+        }
+    }
+}
